c67d8e1691fab42949553fc09e6261061bbaf497
Linykeer Almeida <linykeerrenato@gmail.com>
2023-01-02 14:42:31 -0300

    Hello World getx

---
+++ android/app/src/main/kotlin/br/com/lasistem/jornadagetx/getx_iniciando/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.lasistem.jornadagetx.getx_iniciando
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_iniciando/pages/page1.dart';
+import 'package:getx_iniciando/pages/page2.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Get.to(const Page1());
+            },
+            child: const Text('Page1'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Get.to(const Page2());
+            },
+            child: const Text('Page2'),
+          ),
+        ],
+      )),
+    );
+  }
+}
